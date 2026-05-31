8fa9864669ed6bc4ccadd3836ce1e328137744b1
Elvis Nilsson <Nilssonelvis@gmail.com>
2026-05-31 15:30:00 +0200

    Exceptions and Receipt Behaviours

---
+++ api/src/ReceiptScout.Domain/Exceptions/ForbiddenException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReceiptScout.Application.Common.Exceptions;
+
+public class ForbiddenException : Exception
+{
+    public ForbiddenException(string message = "You are not authorized to perform this action.")
+        : base(message) { }
+}
